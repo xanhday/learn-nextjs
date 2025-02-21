3aaf63cfe787c5d6eed866770470775de56700f2
Xanhday2 <anhnguyeenx2406@gmail.com>
2025-02-21 10:58:36 +0700

    update SEO title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
+export const metadata = {
+  title: "Trang chủ | Learn Next.js",
+  description: "Học Next.js dễ dàng",
+};
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
